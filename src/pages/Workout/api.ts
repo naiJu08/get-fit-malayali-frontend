86736a4584804c0bcb4770de3922b3664e651067
Nethra <nethra.inovace@gmail.com>
2025-11-03 12:40:13 +0530

    completed workout CRUD

---
+++ src/pages/Workout/api.ts
@@ -0,0 +1,115 @@
+import { useQuery } from '@tanstack/react-query'
+import { useMutation } from '@tanstack/react-query'
+
+import {
+  getData,
+  postData,
+  updateFromData,
+  deleteData,
+} from '../../apis/api.helpers'
+import apiUrl from '../../apis/api.url'
+import { QueryParams } from '../../common/types'
+import { useSnackbarManager } from '../../components/common/snackbar'
+import { getErrorMessage, parseQueryParams } from '../../utilities/parsers'
+
+// Disable non-login APIs (employees, groups) for this build
+export const DISABLE_NONLOGIN_APIS = false
+
+const buildUrlWithParams = (baseUrl: string, params: QueryParams) => {
+  return `${baseUrl}${parseQueryParams(params)}`
+}
+
+const fetchData = async (input: QueryParams) => {
+  const url = buildUrlWithParams(apiUrl.WORKOUTS, {
+    ...input,
+  })
+  const response = await getData(url)
+  return response
+}
+export const useWorkoutList = (input: QueryParams) => {
+  return useQuery(['workout_list', input], () => fetchData(input))
+}
+export const deActivateAdmin = (id?: string) => {
+  return updateFromData(`${apiUrl.ADMIN_USER}/${id}/status`, {})
+}
+export const deleteAdmin = (id?: string) => {
+  return deleteData(`${apiUrl.ADMIN_USER}/${id}`)
+}
+export const getWorkoutDetails = (id: string) => {
+  return getData(`${apiUrl.WORKOUTS}/${id}`)
+}
+
+export const freezeUser = (
+  id: string,
+  payload: { reason: string; start_date: string; end_date: string }
+) => {
+  return postData(`${apiUrl.ADMIN_USER}/${id}/freeze`, payload)
+}
+
+export const unfreezeUser = (id: string) => {
+  return postData(`${apiUrl.ADMIN_USER}/${id}/unfreeze`, {})
+}
+
+export const createWorkout = (input: any) => {
+  return postData(`${apiUrl.WORKOUTS}`, input)
+}
+
+export const useCreateWorkout = (handleSubmission: (data: any) => void) => {
+  const { enqueueSnackbar } = useSnackbarManager()
+  return useMutation(createWorkout, {
+    onSuccess: (res: any) => {
+      handleSubmission(res.data)
+      enqueueSnackbar('Workout created successfully', { variant: 'success' })
+    },
+
+    onError: (error: any) => {
+      enqueueSnackbar(
+        getErrorMessage(
+          error.response.data.error || error?.response?.data?.detail
+        ),
+        {
+          variant: 'error',
+        }
+      )
+    },
+  })
+}
+export const updateWorkout = ({ id, data }: any) => {
+  return updateFromData(`${apiUrl.WORKOUTS}/${id}`, data)
+}
+export const useUpdateWorkout = (handleSubmission: (data: any) => void) => {
+  const { enqueueSnackbar } = useSnackbarManager()
+  return useMutation(updateWorkout, {
+    onSuccess: (res: any) => {
+      handleSubmission(res.data)
+      enqueueSnackbar('Workout updated successfully', { variant: 'success' })
+    },
+
+    onError: (error: any) => {
+      // enqueueSnackbar(getErrorMessage(error.response.data.error), {
+      //   variant: 'error',
+      // })
+      enqueueSnackbar(
+        error?.response?.data?.detail
+          ? getErrorMessage(error?.response?.data?.detail)
+          : error?.response?.data?.message,
+        {
+          variant: 'error',
+        }
+      )
+    },
+  })
+}
+export const getRoles = () => {
+  return Promise.resolve({ items: [], total: 0 })
+}
+export const updatePassword = (employee: string, data: string) => {
+  return updateFromData(
+    `${apiUrl.ADMIN_USER}/${employee}/change_password`,
+    data
+  )
+}
+
+export const sendAdminInvitation = (id?: string) => {
+  return postData(`${apiUrl.ADMIN_USER}/${id}/invite`, {})
+}
--- src/routes/mainRoutes.tsx
+++ src/routes/mainRoutes.tsx
@@ -18,6 +18,7 @@ const ForgetPassword = lazy(
 )
 
 const AdminUser = lazy(() => import('../pages/AdminUser'))
+const Workout = lazy(() => import('../pages/Workout'))
 const Plans = lazy(() => import('../pages/Plans'))
 const PlanDetails = lazy(() => import('../pages/Plans/Details/index'))
 const WorkoutPlanDetails = lazy(
@@ -27,6 +28,7 @@ const DietPlanDetails = lazy(
   () => import('../pages/Plans/Details/DietPlan/details')
 )
 const UserDetails = lazy(() => import('../pages/AdminUser/Details'))
+const WorkoutDetails = lazy(() => import('../pages/Workout/Details'))
 
 // Dashboard
 
@@ -43,15 +45,17 @@ const routes: any = [
   { slug: 'DASHBOARD', component: <Dashboard /> },
   { slug: 'SETTINGS_SAMPLE', component: <Settings /> },
   { slug: 'ADMIN_USER', component: <AdminUser /> },
+  { slug: 'WORKOUT', component: <Workout /> },
   { slug: 'PLANS', component: <Plans /> },
   { slug: 'PLAN_DETAILS', component: <PlanDetails /> },
-  { slug: 'WORKOUT_DETAILS', component: <WorkoutPlanDetails /> },
+  { slug: 'WORKOUT_PLAN_DETAILS', component: <WorkoutPlanDetails /> },
   { slug: 'DIET_DETAILS', component: <DietPlanDetails /> },
   { slug: 'SUBSCRIPTION_SAMPLE', component: <Subscription /> },
   { slug: 'DISCOUNT_SAMPLE', component: <Discount /> },
   { slug: 'PAYMENT_SAMPLE', component: <Payment /> },
   { slug: 'EXPORT_SAMPLE', component: <ExportPage /> },
   { slug: 'SUPPORT_SAMPLE', component: <Support /> },
+  { slug: 'WORKOUT_DETAILS', component: <WorkoutDetails /> },
 
   {
     isAuthRoute: true,
@@ -75,6 +79,7 @@ const routes: any = [
   // },
 
   { slug: 'ADMIN_USER', component: <AdminUser /> },
+  { slug: 'WORKOUT_DETAILS', component: <WorkoutDetails /> },
 ]
 
 export default function MainRoutes() {
